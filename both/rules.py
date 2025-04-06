1e303bd84c0a04a4cf62bfaaae88b37579d157da
David <58551142+D-G-Tomlinson@users.noreply.github.com>
2025-04-06 11:15:47 +0100

    most files

---
+++ both/rules.py
@@ -0,0 +1,139 @@
+# RULES.PY
+# Generates Scrabble Banter-Style rules
+# Checks words follow rules
+
+# REQUIRES words.txt in operating directory
+
+# Usage guide:
+# Initiate rule with Rule()
+# EITHER:
+#     Create from parameters with create_rule(type1, letters1, type2, letters2)
+#     OR Generate randomly with generate_rule(difficulty)
+# Check words with rule.check_word()
+# Export rule to tuple with rule.to_tuple()
+
+import random
+import numpy as np
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+rules_str = ["Starts with", "Must have", "Can't have", "Ends in"]
+
+def get_words():
+    f = open("words.txt", "r")
+    s = f.read().lower()
+    return s.split("\n")
+    
+scrabble_words = get_words()
+scrabble_word_count = len(scrabble_words)
+
+def random_letters():
+    num = random.randint(1,4)
+    if (num == 1):
+        return (letters[random.randint(0,25)] + letters[random.randint(0,25)])
+    else:
+        return letters[random.randint(0,25)]
+
+def check_single_rule(word, rule_type, rule_letters):
+    if (rule_type == 0):
+        return word.startswith(rule_letters)
+    elif (rule_type == 1):
+        return (word.count(rule_letters) > 0)
+    elif (rule_type == 2):
+        return (word.count(rule_letters) == 0)
+    elif (rule_type == 3):
+        return word.endswith(rule_letters)
+
+def create_rule(type1, letters1, type2, letters2):
+    rule = Rule()
+    rule.set(type1, letters1, type2, letters2)
+    return rule
+
+def generate_rule(difficulty):
+    rule = Rule()
+    rule.generate(difficulty)
+    return rule
+
+class Rule:
+    
+    def __init__(self):
+        self.rule_1_type = 0
+        self.rule_2_type = 0
+        self.rule_1_letters = "a"
+        self.rule_2_letters = "a"
+
+    def __str__(self):
+        return (rules_str[self.rule_1_type] + " \"" + self.rule_1_letters.upper() + "\"\n" + rules_str[self.rule_2_type] + " \"" + self.rule_2_letters.upper() + "\"")
+
+    def set(self, rule_1_type, rule_1_letters, rule_2_type, rule_2_letters):
+        self.rule_1_type = rule_1_type
+        self.rule_1_letters = rule_1_letters.lower()
+        self.rule_2_type = rule_2_type
+        self.rule_2_letters = rule_2_letters.lower()
+
+    def to_tuple(self):
+        return (self.rule_1_type, self.rule_1_letters, self.rule_2_type, self.rule_2_letters)
+
+    #Gives this "Rule" object a random pair of word rules
+    def generate_random(self):
+        num = random.randint(1,6)
+        if (num == 1):
+            self.rule_1_type = 0
+            self.rule_2_type = 1
+        elif (num == 2):
+            self.rule_1_type = 0
+            self.rule_2_type = 2
+        elif (num == 3):
+            self.rule_1_type = 0
+            self.rule_2_type = 3
+        elif (num == 4):
+            self.rule_1_type = 1
+            self.rule_2_type = 2
+        elif (num == 5):
+            self.rule_1_type = 1
+            self.rule_2_type = 3
+        elif (num == 6):
+            self.rule_1_type = 2
+            self.rule_2_type = 3
+        self.rule_1_letters = random_letters()
+        self.rule_2_letters = random_letters()
+
+    def check_word(self, word):
+        word = word.lower()
+        return (check_single_rule(word, self.rule_1_type, self.rule_1_letters) and check_single_rule(word, self.rule_2_type, self.rule_2_letters))
+
+    def check_word_2(self, word):
+        word = word.lower()
+        return (check_single_rule(word, self.rule_1_type, self.rule_1_letters), check_single_rule(word, self.rule_2_type, self.rule_2_letters))
+
+    def evaluate_difficulty(self):
+        counts = np.zeros(4) #Neither, First, Second, Both
+        for word in scrabble_words:
+            result = self.check_word_2(word)
+            if (result[0] and result[1]):
+                counts[3] += 1
+            else:
+                if (result[0]):
+                    counts[1] += 1
+                elif (result[1]):
+                    counts[2] += 1
+                else:
+                    counts[0] += 1
+        frequencies = counts * 100 / scrabble_word_count
+        return frequencies
+
+    def is_reasonable(self, difficulty):
+        frequencies = self.evaluate_difficulty()
+        min_freq = 1/difficulty
+        max_freq = 40/difficulty
+        if (frequencies[1] > 0.00001 and frequencies[1] < 50 and frequencies[2] > 0.00001 and frequencies[2] < 50 and frequencies[3] > min_freq and frequencies[3] < max_freq):
+            return True
+        return False
+
+    def generate(self, difficulty):
+        rule_valid = False
+        while (not rule_valid):
+            self.generate_random()
+            if (self.is_reasonable(difficulty)):
+                rule_valid = True
+
+
---
+++ server/app.py
@@ -0,0 +1,174 @@
+from flask import Flask, request, abort, jsonify
+import random
+import datetime
+
+import rules
+
+MAX_ROUNDS = 10
+BETWEEN = 5
+class Round:
+    length = 15
+    def __init__(self,prev_val):
+        if prev_val==MAX_ROUNDS:
+            self.rule=None
+            self.num=None
+            self.finished=None
+            return
+        self.num = prev_val + 1
+        self.rule = rules.generate_rule(self.num).to_tuple()
+        self.finished = None
+    def finish(self):
+        self.finished=datetime.datetime.now()
+    def ready_j(self):
+        fin = self.finished != None
+        return {"rule":self.rule,"num":self.num,"length":Round.length,"finished":fin}
+
+games = {}
+
+
+class Game:
+    def __init__(self,player1):
+        self.code = random.randint(10000,99999)
+        while self.code in games:
+            self.code = random.randint(10000,99999)
+        self.player_scores = {player1:(0,1)}
+        self.r = None
+    def start(self):
+        self.r = Round(0)
+    def guess(self, player, in_round, score):
+        if player not in self.player_scores:
+            abort(404, description="that player is not in this game")
+        if in_round!=self.r.num:
+            print(f"Round is {self.r.num}, player round is {in_round}")
+            abort(403, description=f"Round is {self.r.num}, player round is {in_round}")
+        p_vals = self.player_scores[player]
+        if in_round!=p_vals[1]:
+            abort(403, description="already guessed or summat")
+        self.player_scores[player] = (p_vals[0] + score, p_vals[1]+1)
+        if self.test_all_players(self.r.num+1):
+            self.next_round(player)
+        
+    def test_all_players(self,val):
+        ready = True
+        for _,r in self.player_scores.values():
+            if r!=(val):
+                ready = False
+        return ready
+    def next_round(self,player):
+        if self.r.num==MAX_ROUNDS:
+            self.r = Round(MAX_ROUNDS)
+            self.player_finished(player)
+        else:
+            self.r.finish()
+    def ready_j(self):
+        r = None if self.r == None else self.r.ready_j()
+        return {"code":self.code,
+                "players":self.player_scores,
+                "round":r}
+    def player_finished(self,player):
+        if player in self.player_scores:
+            self.player_scores[player]=(self.player_scores[player][0],-1)
+            if self.test_all_players(-1):
+                games.pop(self.code)
+        else:
+            abort(403, description="that player don't exist lol")
+    
+def get_game(game_code):
+    try:
+        game_code = int(game_code)
+    except:
+        abort(403,description="thats not an integer")
+    if game_code not in games:
+        abort(403, description="that game doesn't exist")
+    return games[game_code]
+    
+        
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello from David's Flask!"
+
+@app.delete('/game')
+def delete_game():
+    global games    
+    todel=request.args.get("delete")
+    if todel == None:
+        games = {}
+        return "Success",200    
+    try:
+        todel = int(todel)        
+        games.pop(todel)
+        return "Success",200
+    except:
+        abort(403, description="that game doesn't exist")
+@app.delete('/game/leave')
+def remove_player():
+    g = get_game(request.args.get("game_code"))
+    userid = str(request.args.get("userid"))
+    if userid not in g.player_scores:
+        abort(403, description="youre not in")
+    g.player_scores.pop(userid)
+    if len(g.player_scores)==0:
+        games.pop(g.code)
+    return "Success",200
+    
+
+@app.get('/game')
+def get_games():
+    return jsonify({"games":list(games.keys())}),200
+
+@app.patch('/game/guess')
+def submit_guess():
+    try:
+        game_code=request.args.get("game_code")
+        player=request.args.get("userid")
+        in_round=int(request.args.get("round"))
+        score=int(request.args.get("score"))        
+    except:
+        abort(403, description="error taking in arguments")
+    g = get_game(game_code)
+    g.guess(player,in_round,score)
+    return jsonify({"game_data":g.ready_j()}),200
+
+@app.patch('/game/join')
+def join_game():
+    g = get_game(request.args.get("game_code"))
+    userid = str(request.args.get("userid"))
+    if g.r!=None:
+        abort(403, description="game is running")
+    if userid in g.player_scores:
+        abort(403, description="youre already in")
+    if len(g.player_scores)>=5:
+        abort(403, description="too many in lobby")
+    g.player_scores[userid]=0
+    return "Success",200
+
+@app.patch('/game/start')
+def start_game():
+    g = get_game(request.args.get("game_code"))
+    g.start()
+    return jsonify({"game_data":g.ready_j()}),200
+
+@app.get('/game/arewethereyet')
+def get_update():
+    g = get_game(request.args.get("game_code"))
+    r = g.r
+    if r!=None:
+        if r.finished!=None:
+            if (datetime.datetime.now()-r.finished).total_seconds()>5:
+                g.r = Round(r.num)
+        elif r.rule==None and r.num==None:
+            userid = request.args.get("userid")
+            g.player_finished(userid)
+    return jsonify({"game_data":g.ready_j()}),200
+
+@app.post('/game')
+def new_game():
+    userid = str(request.args.get("userid"))
+    if userid == None:
+        abort(400, description="no userid provided")
+    ng = Game(userid)
+    code = ng.code
+    games[code]=ng
+    return jsonify({"gameCode":code}),201
